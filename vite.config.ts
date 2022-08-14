673bd869ecf2bdb7b1d386e59fe98e9494c343f7
Kwanhooo <kwanhooo@foxmail.com>
2022-08-14 15:34:53 +0800

    fix: 尝试修复Vercel的404问题

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
  * https://vitejs.dev/config/
  */
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     Components({
